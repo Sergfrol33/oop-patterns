2a6a616ec8f44a1aeb0ef10640118868e0f208f5
Sergfrol33 <56001868+Sergfrol33@users.noreply.github.com>
2021-10-11 20:26:09 +0300

    checkout

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = {
     mode: "development",
     devServer: {
@@ -10,15 +11,19 @@ module.exports = {
         progress: true,
         hot: true,
         open: true,
-        historyApiFallback: true,
     },
+    target: 'web',
     entry: './src/index.js',
     output: {
         path: path.join(__dirname,'dist'),
-        filename: "index_bundle.js"
+        filename: "bundle.js"
     },
     module: {
         rules: [
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset'
+            },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
@@ -27,14 +32,19 @@ module.exports = {
                 }
             },
             {
-                test: /\.css$/,
-                use: ['style-loader','css-loader']
+                test: /\.(s[ac])ss$/i,
+                use: ['css-loader','postcss-loader','sass-loader']
             }
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/public/index.html"
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './src/assets', to: 'assets' }
+            ]
         })
     ],
 }
