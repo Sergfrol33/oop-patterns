2693b1990cb6958df51025311a52c7fa5e98e341
Sergfrol33 <56001868+Sergfrol33@users.noreply.github.com>
2021-10-11 22:48:43 +0300

    state pattern

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,49 @@
-import './test.js'
 abstract class State {
     protected context: Context | undefined
-    public setContext(context:Context){
+
+    public setContext(context: Context) {
         this.context = context
     }
-    public abstract handle1():void
-    public abstract handle2():void
+
+    public abstract switchColor(): void
 }
 
 class Context {
     private state: State | undefined
+    private button: HTMLButtonElement | null = document.querySelector('button')
+
     constructor(state: State) {
         this.transitionTo(state)
     }
-    public transitionTo(state:State):void{
-        console.log(`Context: Transition to ${(<any>state).constructor.name}.`);
+
+    public transitionTo(state: State): void {
         this.state = state
         this.state.setContext(this)
     }
-    public request1(){
-        this.state?.handle1()
-    }
-    public request2(){
-        this.state?.handle2()
-    }
-}
-class ConcreteStateA extends State{
-    public handle1() {
-        console.log('request1 ConcreteStateA')
-        this.context?.transitionTo(new ConcreteStateB())
-    }
-    public handle2() {
-        console.log('handle request2')
+
+    public addButtonListener() {
+        this.button?.addEventListener('click', () => {
+            this.state?.switchColor()
+        })
     }
 }
-class ConcreteStateB extends State{
-    public handle1() {
-        console.log('request1 ConcreteStateB')
-        this.context?.transitionTo(new ConcreteStateA())
+
+class BlackColor extends State {
+    public switchColor(): void {
+        document.body.classList.remove('red')
+        document.body.classList.add('black')
+        this.context?.transitionTo(new RedColor())
     }
-    public handle2() {
-        console.log('handle request2  ConcreteStateB')
+
+}
+
+class RedColor extends State {
+    public switchColor(): void {
+        document.body.classList.remove('black')
+        document.body.classList.add('red')
+        this.context?.transitionTo(new BlackColor())
     }
 }
-const context = new Context(new ConcreteStateA())
-context.request1()
-context.request2()
+
+const context = new Context(new BlackColor())
+context.addButtonListener()
